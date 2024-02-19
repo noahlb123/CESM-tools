d6a66b2d68d3dfa1758c2e9b75a1d8ded59d8a05
noahlb123 <noahliguoribills@gmail.com>
2024-02-19 15:22:19 -0500

    Made identiychecker output more readable and added better comparison techniques

--- global-plot.py
+++ global-plot.py
@@ -1,51 +1,98 @@
 import cartopy
 import matplotlib.pyplot as plt
+import plotly.express as px
 import pandas as pd
 
+class IdenticalManager:
+    def __init__(self):
+        self.d = {}
+    
+    def get_key(self, vector):
+        return str(vector[0]) + "," + str(vector[1])
+
+    def add(self, root, new):
+        key = self.get_key(root)
+        if key in self.d:
+            self.d[key].append(new)
+        else:
+            self.d[key] = [root, new]
+    
+    def print(self):
+        for value in self.d.values():
+            print(value)
+
 class IdentityChecker:
-    def __init__(self, threshold):
+    def __init__(self, threshold, ingnores=set()):
         self.t = threshold
         self.l = []
-        self.identicals = []
+        self.identicals = IdenticalManager()
+        self.n_unique = 0
+        self.ignores = ingnores
     
-    def is_unique(self, latlon, notes):
+    def is_unique_single(self, latlon):
         latlon = latlon[1:3] if len(latlon) == 4 else latlon
         for v in self.l:
+            if (v[0] in self.ignores and latlon[0] in self.ignores) or (v[1] in self.ignores and latlon[1] in self.ignores):
+                continue
             if abs(v[0] - latlon[0]) < self.t or abs(v[1] - latlon[1]) < self.t:
-                self.identicals.append([latlon, v, notes])
+                self.identicals.add(v, latlon)
                 return False
         self.l.append(latlon)
+        self.n_unique += 1
         return True
     
+    def is_unique(self, latlon):
+        if type(latlon[0]) == type(pd.Series([])):
+            #if list of latlons
+            output = latlon[0].copy()
+            for i in latlon[0].index:
+                output[i] = (self.is_unique_single([latlon[0][i], latlon[1][i]]))
+            return pd.Series(output)
+        elif type(latlon[0]) == type(1):
+            #if sinlg latlon
+            return self.is_unique_single(vector)
+        else:
+            raise Exception(type(latlon[0]), "doesn't match either ", type(1), " or ", type(pd.Series([])))
+    
     def results(self):
-        return self.identicals
+        self.identicals.print()
 
-checker = IdentityChecker(0.00001)
-ax = plt.axes(projection=cartopy.crs.PlateCarree(0))
-ax.set_extent([-180, 180, -90, 90], crs=cartopy.crs.PlateCarree())
-
-#get this from https://www.naturalearthdata.com/features/
-glaciers = cartopy.feature.NaturalEarthFeature(
-    category='physical',
-    name='glaciated_areas',
-    scale='10m',
-    facecolor='#ceeefb')
-ax.add_feature(cartopy.feature.COASTLINE, edgecolor='grey')
-ax.add_feature(glaciers)
-
-
+#Sigl 2019 and Brugger 2021 are 0.000691 apart
+#They must be manually ignored because they too close for float comparison
+checker = IdentityChecker(0.0005, set([45.93056, 45.932]))
 p = pd.read_csv('data/ice-core-data.csv').dropna(subset=["S"])
+#remove duplicate ice core locations
+print(len(p), "total ice core datasets")
+p = p[checker.is_unique([p["N"], p["E"]])]
 coords = p.loc[:, ["N", "S", "E", "W"]].to_numpy()
-latlons = [[-75, 43], [77.23, 28.61]]
+print(len(p), "unique ice core locations")
 
-counter = 0
-for i in range(len(coords)):
-    vector = coords[i]
-    if checker.is_unique(vector, ""):
-        counter += 1
-        plt.plot(vector[2], vector[1], color='black', marker='o')
+inp = input("Plotly or Cartopy? (p/c): ")
+if (inp == "c"):
+    #Matplot
+    ax = plt.axes(projection=cartopy.crs.Robinson())
+    ax.set_extent([-180, 180, -90, 90], crs=cartopy.crs.PlateCarree())
 
-# Save the plot by calling plt.savefig() BEFORE plt.show()
-#plt.savefig('coastlines.png')
-plt.show()
-print(counter, len(checker.results()), len(coords))
+    #get this from https://www.naturalearthdata.com/features/
+    glaciers = cartopy.feature.NaturalEarthFeature(
+        category='physical',
+        name='glaciated_areas',
+        scale='10m',
+        facecolor='#00A6E3')
+    ax.add_feature(cartopy.feature.COASTLINE, edgecolor='grey')
+    ax.add_feature(glaciers)
+
+    for i in range(len(coords)):
+        vector = coords[i]
+        plt.plot(vector[2], vector[1], color='black', marker='.', markersize=3, transform=cartopy.crs.PlateCarree())
+
+    plt.savefig('figures/ice-core-locations.png', dpi=300)
+    plt.show()
+elif (inp == "p"):
+    #Plotly
+    fig = px.scatter_geo(p, lat='N', lon='E', hover_name='First Author', title='Ice Core Locations')
+    #color='Earliest Year (CE)'
+    fig.show()
+
+print("duplicate locations:")
+checker.results()
