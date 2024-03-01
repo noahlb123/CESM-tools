944ffc18c7788848c76b6d7b8a9979d3d6462c46
noahlb123 <noahliguoribills@gmail.com>
2024-03-01 11:54:01 -0500

    Added box plot, discretized color bar

--- pi-pd.py
+++ pi-pd.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from matplotlib.lines import Line2D
 from matplotlib.cm import ScalarMappable
-from matplotlib.colors import Normalize
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 from matplotlib import colormaps
+import plotly.express as px
 import numpy as np
 import cartopy
 import scipy
@@ -41,7 +43,8 @@ for index, row in p.iterrows():
         #add data to datasets
         if (y1 != None and y2 != None and abs(y1 - y2) >= 100 and y1 < 1900):
             for key in windows:
-                full_data[key].append([filename, lat, lon, a1[key], a2[key], a2[key]/a1[key], y1, y2, a3[key], y3])
+                if filename != "legrand-2023-1.csv":
+                    full_data[key].append([filename, lat, lon, a1[key], a2[key], a3[key]/a1[key], y1, y2, a3[key], y3])
             #cartopy
             for_cartopy[filename] = {'lat': lat, 'lon': lon, 'ratio': a3[5] / a1[5]}
 
@@ -49,7 +52,7 @@ for index, row in p.iterrows():
 inp = input("Matplot or Cartopy? (m/c): ")
 def format_column(c):
     return np.transpose(c).astype('float64').tolist()[0]
-if (inp == "m"):#Raw Matplot
+if (inp == "m"): #Raw Matplot
     for target_w in windows:
         #setup data
         data = np.matrix(full_data[target_w])
@@ -85,20 +88,42 @@ if (inp == "m"):#Raw Matplot
         t.matplot_tooltips(ax, fig, sc, annotations)
         #ax.legend(handles=legend_elements, loc='upper left')
         plt.legend(handles=legend_elements)
-        plt.savefig("figures/ice-cores/" + str(target_w))
-        #plt.show()
+        #plt.savefig("figures/ice-cores/" + str(target_w))
+        plt.show()
         plt.close()
-elif (inp == "c"):#Cartopy
+elif (inp == 'b'): #box plot
+    for target_w in [5]:
+        data = np.matrix(full_data[target_w])
+        my_pi = [math.log(x, 10) for x in format_column(data[:,3])]
+        my_pd = [math.log(x, 10) for x in format_column(data[:,4])]
+        #my_pi = format_column(data[:,3])
+        #my_pd = format_column(data[:,4])
+        # rectangular box plot
+        fig, ax = plt.subplots()
+        bplot1 = ax.boxplot([my_pi, my_pd], vert=True, labels=[1850, 1980])
+        #ax.set_title('Rectangular box plot')
+        ax.yaxis.grid(True)
+        ax.set_xlabel('Year')
+        ax.set_ylabel('log10(PD/PI BC Conc.)')
+        #ax.set_ylabel('PD/PI BC Conc.')
+        #plt.ylim(0, 25)
+        plt.savefig('figures/ice-cores/test-box-1.png', dpi=300)
+        plt.show()
+elif (inp == 'p'): #Plotly
+    fig = px.scatter_geo(p, lat='N', lon='E', hover_name='First Author', title='PD/PI Ratios')
+    fig.show()
+elif (inp == "c"): #Cartopy
     #Matplot
     #f, ax = plt.subplots(projection=cartopy.crs.Robinson())
-    ax = plt.axes(projection=cartopy.crs.Robinson())
-    ax.set_extent([-180, 180, -90, 90], crs=cartopy.crs.PlateCarree())
+    #ax = plt.axes(projection=cartopy.crs.Robinson())
+    ax = plt.axes(projection=cartopy.crs.RotatedPole())
+    #ax.set_extent([-180, 180, -90, 90], crs=cartopy.crs.PlateCarree())
 
     #get this from https://www.naturalearthdata.com/features/
     glaciers = cartopy.feature.NaturalEarthFeature(
         category='physical',
         name='glaciated_areas',
-        scale='10m',
+        scale='110m',
         facecolor='#00A6E3')
     ax.add_feature(cartopy.feature.COASTLINE, edgecolor='grey')
     ax.add_feature(glaciers)
@@ -115,9 +140,17 @@ elif (inp == "c"):#Cartopy
         #r = math.log(for_cartopy[key]['ratio'], 10)
         r = for_cartopy[key]['ratio']
         max_ratio = r if r > max_ratio else max_ratio
-    #norm = Normalize(vmin=-max_ratio, vmax=max_ratio)
-    norm = Normalize(vmin=0, vmax=2)
-    cmap = colormaps['PRGn']#inferno
+    cmap = colormaps['BrBG_r']#inferno
+    # extract all colors from the map
+    cmaplist = [cmap(i) for i in range(cmap.N)]
+    # force the last color entry to be red
+    #cmaplist[-1] = (1, 0, 0, 1.0)
+    # create the new map
+    cmap = LinearSegmentedColormap.from_list(
+        'Custom cmap', cmaplist, cmap.N)
+    # define the bins and normalize
+    bounds = [round(x, 1) for x in np.linspace(0, 2, 8)]
+    norm = BoundaryNorm(bounds, cmap.N)
     sm = ScalarMappable(cmap=cmap, norm=norm)
 
     #plot
@@ -126,11 +159,13 @@ elif (inp == "c"):#Cartopy
         [lat, lon] = [obj['lat'], obj['lon']]
         if math.isnan(lat) or math.isnan(lon):
             lat, lon = dup_index_map[key]
-        plt.plot(lon, lat, c=cmap(norm(obj['ratio'])), markeredgecolor='black', marker='^', markersize=7, transform=cartopy.crs.PlateCarree())
-        #plt.plot(lon, lat, c=cmap(norm(math.log(obj['ratio'], 10))), markeredgecolor='black', marker='^', markersize=6, transform=cartopy.crs.PlateCarree())
+        temp = key.split('-')
+        print(temp[0].capitalize() + " et al. " + temp[1], lat, lon, round(obj['ratio'], 3), temp[2])
+        plt.plot(lon, lat, c=cmap(norm(obj['ratio'])), markeredgecolor='black', marker='.', markersize=9, transform=cartopy.crs.PlateCarree())
+        #plt.plot(lon, lat, c=cmap(norm(math.log(obj['ratio'], 10))), markeredgecolor='black', marker='.', markersize=6, transform=cartopy.crs.PlateCarree())
     plt.colorbar(mappable=sm, label="PD/PI BC Conc.", orientation="horizontal")
     
-    plt.savefig('figures/ice-cores/diverging-global-ratios-1980.png', dpi=300)
-    #plt.show()
+    #plt.savefig('figures/ice-cores/discrete-global-ratios-1980.png', dpi=300)
+    plt.show()
 
-#index does not have lat lons of multiple ice cores from the same pub, plots them all at 0, 0
+print("n=" + str(len(for_cartopy)))
