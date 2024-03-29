9deacce6269dd627f4b06f502a123adfba184d47
noahlb123 <noahliguoribills@gmail.com>
2024-03-29 14:14:06 -0400

    added terminal output with some useful unix scripts

--- scp-command-generator.py
+++ scp-command-generator.py
@@ -1,5 +1,5 @@
 print("scp 'nlbills@derecho.hpc.ucar.edu:/glade/campaign/cesm/collections/cesmLE/restarts/{", end='')
-for i in range(1, 36):
+for i in range(8, 36):
     print("b.e11.B20TRC5CNBDRD.f09_g16.0" + "{:02d}".format(i) + "/b.e11.B20TRC5CNBDRD.f09_g16.0" + "{:02d}".format(i) + ".rest.1980-01-01-00000.tar", end='')
 
     if (i != 35):
