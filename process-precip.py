e7d6a0fc8fdd3d10fb6847cbeada474df9092289
noahlb123 <noahliguoribills@gmail.com>
2024-07-15 20:06:10 -0600

    Update process-precip.py

--- process-precip.py
+++ process-precip.py
@@ -94,12 +94,14 @@ for i in range(len(filenames)):
     f = Dataset(root + '/' + file_name)
     print(f.variables['lat'].shape[0], f.variables['lon'].shape[0], file_name)
     if f.variables['lat'].shape[0] > 64 or f.variables['lon'].shape[0] > 128:
+        to_eval += 'echo "regridding ' + file_name + '" && '
         to_eval += "ncap2 -O -s 'prsn=double(prsn);' " + file_name + ' ' + file_name + ' && '
         to_eval += 'ncremap -d CanESM5-1.nc ' + file_name + ' ' + file_name.replace('.nc', '_re.nc') + ' && '
         filenames[i] = file_name.replace('.nc', '_re.nc')
     f.close()
 
 #comand to average files
+to_eval += 'echo "doing final step..." && '
 to_eval += 'ncra ' + ' '.join(filenames) + ' output.nc -O'
 
 #evaluate
