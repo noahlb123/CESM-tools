58ac07e8f2a876a73b835a1a0870cf3aa4c69c03
noahlb123 <noahliguoribills@gmail.com>
2024-07-30 14:11:45 -0400

    lowercase cmip directories

--- cmip.py
+++ cmip.py
@@ -177,10 +177,13 @@ for era, year in sheets.items():
         [fields.append(name) for name in ice_coords.keys()]
         filename = data_type + '.csv' if data_type != 'main' else era + '.csv'
         subfolder = target_model if target_model != 'CESM' else target_model + '-' + target_v.replace('wetbc', 'wetdry')
+        subfolder = subfolder.lower()
         write_path = os.path.join(os.getcwd(), 'data', 'model-ice-depo', subfolder, filename)
         with open(write_path, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fields)
             writer.writeheader()
             writer.writerows(csv_dict)
+
+#loadbc
 
 print("done.")
