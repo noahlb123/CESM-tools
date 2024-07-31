02032250ea199edda00b21db1e44b615026d4227
noahlb123 <noahliguoribills@gmail.com>
2024-07-31 13:32:56 -0400

    Update model-timeseries.py

--- model-timeseries.py
+++ model-timeseries.py
@@ -2,6 +2,13 @@ from netCDF4 import Dataset
 import pandas as pd
 import os
 
+#get files from each model
 lens_files = pd.DataFrame(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'lens', 'fileuse-index.csv'))
 cmip_files = pd.DataFrame(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'cmip6', 'fileuse-index.csv'))
 sootsn_files = pd.DataFrame(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'cesm-sootsn', 'fileuse-index.csv'))
+
+#combine files into one dataframe
+files = pd.DataFrame(columns=['files'])
+files['files'] = lens_files['files'] + cmip_files['wet file'] + cmip_files['dry file'] + sootsn_files['wet file']
+
+print(files)
