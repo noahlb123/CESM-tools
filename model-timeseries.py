60d67f475beb8a8ce76b9d2daeec21739fa48506
noahlb123 <noahliguoribills@gmail.com>
2024-07-31 14:25:18 -0400

    Update model-timeseries.py

--- model-timeseries.py
+++ model-timeseries.py
@@ -7,14 +7,14 @@ lens_files = pd.read_csv(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'le
 cmip_files = pd.read_csv(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'cmip6', 'fileuse-index.csv'))
 sootsn_files = pd.read_csv(os.path.join(os.getcwd(), 'data', 'model-ice-depo', 'cesm-sootsn', 'fileuse-index.csv'))
 
-print('lens_files')
+'''print('lens_files')
 for f in lens_files['files']:
     print(f)
 print('cmip_files')
 for f in cmip_files['wet file']:
-    print(f)
-print('sootsn_files')
-for f in sootsn_files['wet file']:
+    print(f)'''
+print('cmip_files dry')
+for f in cmip_files['wet file']:
     print(f)
 
 
@@ -22,5 +22,5 @@ for f in sootsn_files['wet file']:
 files = pd.DataFrame(columns=['files'])
 files['files'] = lens_files['files'] + cmip_files['wet file'] + cmip_files['dry file'] + sootsn_files['wet file']
 
-print(files)
+#print(files)
 #print(sootsn_files)
