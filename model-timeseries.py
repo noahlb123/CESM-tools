c1389cb69a93b2d2d388919aee45f69eb49be28c
noahlb123 <noahliguoribills@gmail.com>
2024-07-31 13:38:56 -0400

    Update model-timeseries.py

--- model-timeseries.py
+++ model-timeseries.py
@@ -12,5 +12,5 @@ files = pd.DataFrame(columns=['files'])
 files['files'] = lens_files['files'] + cmip_files['wet file'] + cmip_files['dry file'] + sootsn_files['wet file']
 
 print(files)
-print(cmip_files['wet file'])
+print(lens_files['files'] + cmip_files['wet file'] + cmip_files['dry file'])
 #print(sootsn_files)
