70c7e38d7e9fcf71c52642fcdae858a6cf9f894a
noahlb123 <noahliguoribills@gmail.com>
2024-08-01 15:33:20 -0400

    Create get-smallest-nc-file.py

---
+++ get-smallest-nc-file.py
@@ -0,0 +1,17 @@
+from netCDF4 import Dataset
+import sys
+import os
+
+if len(sys.argv) < 2:
+    raise Exception('1 command line argument required: <root directory>')
+dir = sys.argv[1]
+min_lat = 999999999
+min_lon = 999999999
+min_name = ''
+
+for filename in os.listdir(dir):
+    if '.nc' in filename
+        f = Dataset(filename)
+        lat = f['lat'].shape
+        lon = f['lon'].shape
+        print(lat, lon)
