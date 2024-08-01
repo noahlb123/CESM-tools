63b393395f2bb5517bd05fe8dcc31839ee9fa7a5
noahlb123 <noahliguoribills@gmail.com>
2024-08-01 11:48:35 -0400

    Update model-timeseries.py

--- model-timeseries.py
+++ model-timeseries.py
@@ -33,7 +33,7 @@ s_lat, s_lon = T.get_ice_coords('data/standardized-ice-cores/index.csv', 'data/s
 
 #get timeseries
 x = [i + 0.5 for i in range(1850, 1981)]
-timeseries = pd.DataFrame(columns=['year'], data=x)
+timeseries = pd.DataFrame(index=['year'], data=x)
 c = 1
 l = len(files.index) - len(lens_files.index)
 for index, row in files.iterrows():
