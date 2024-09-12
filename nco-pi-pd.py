b1cd96e2e54603909978080b1a99800235d2d308
noahlb123 <noahliguoribills@gmail.com>
2024-09-12 11:37:34 -0400

    Update nco-pi-pd.py

--- nco-pi-pd.py
+++ nco-pi-pd.py
@@ -108,20 +108,17 @@ for model_name in valid_er_models:
     pi = model_name + '_pi.nc'
     pd = model_name + '_pd.nc'
     new_name = model_name + '.nc'
-    #to_eval += 'ncbo --op_typ=dvd ' + pd + ' ' + pi + ' ' + new_name + ' -O && '
+    to_eval += 'ncbo --op_typ=dvd ' + pd + ' ' + pi + ' ' + new_name + ' -O && '
 
 filenames = [model_name + '.nc' for model_name in valid_er_models]
 
 #commands to remove time_bnds variable
-#to_eval += 'echo "removing time_bnds variable..." && '
+to_eval += 'echo "removing time_bnds variable..." && '
 for file_name in filenames:
-    #to_eval += 'ncks -C -O -x -v time_bnds ' + file_name + ' ' + file_name + ' -O && '
-    pass
+    to_eval += 'ncks -C -O -x -v time_bnds ' + file_name + ' ' + file_name + ' -O && '
 
 print('evaluating section 1/2 ...')
-print(to_eval[0:len(to_eval) - 4])
 os.system(to_eval[0:len(to_eval) - 4]) #remove trailing ' && '
-exit()
 to_eval = 'cd ' + root + ' && '
 
 #commands to regrid all models
