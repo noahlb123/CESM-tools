571b9795d9226c2d214d0af5e8fba5697f963d29
noahlb123 <noahliguoribills@gmail.com>
2024-10-04 10:22:43 -0400

    added timeseries to workflow

--- new-workflow.sh
+++ new-workflow.sh
@@ -1,4 +1,9 @@
 python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all &&
 python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all CESM &&
 python3 nco-pi-pd.py sootsn /glade/derecho/scratch/nlbills/cmip6-snow-dep &&
+python3 nco-pi-pd.py loadbc /glade/derecho/scratch/nlbills/cmip-atmos &&
+
+python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all &&
+python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all CESM &&
+python3 nco-pi-pd.py sootsn /glade/derecho/scratch/nlbills/cmip6-snow-dep &&
 python3 nco-pi-pd.py loadbc /glade/derecho/scratch/nlbills/cmip-atmos
