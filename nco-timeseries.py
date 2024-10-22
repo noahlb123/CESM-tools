c8552468ab14e4ae88fc1c729e3a522da8cd3fb9
noahlb123 <noahliguoribills@gmail.com>
2024-10-22 11:21:03 -0400

    Update nco-timeseries.py

--- nco-timeseries.py
+++ nco-timeseries.py
@@ -160,7 +160,8 @@ if do_nco:
         f = Dataset(root + '/' + file_name)
         to_eval += "ncap2 -O -s '" + target_v + "=double(" + target_v + ");' " + file_name + ' ' + file_name + ' && '
         if f.variables['lat'].shape[0] > smallest_lat_lon_shape[0] or f.variables['lon'].shape[0] > smallest_lat_lon_shape[1]:
-            #to_eval += 'rm ' + file_name.replace('.nc', '_re.nc') + ' && '
+            if os.path.isfile(os.path.join(root, file_name.replace('.nc', '_re.nc'))):
+                to_eval += 'rm ' + file_name.replace('.nc', '_re.nc') + ' && '
             to_eval += 'ncremap -d ' + smallest_grid + ' ' + file_name + ' ' + file_name.replace('.nc', '_re.nc') + ' && '
             to_eval += 'ncks -C -O -x -v time_bnds,area,gw ' + file_name.replace('.nc', '_re.nc') + ' ' + file_name.replace('.nc', '_re.nc') + ' -O && '
             filenames[i] = file_name.replace('.nc', '_re.nc')
