70dc888a657534053d0f6fb071c632ed095a5b92
noahlb123 <noahliguoribills@gmail.com>
2025-01-13 16:07:41 -0500

    Update lev_p.py

--- lev_p.py
+++ lev_p.py
@@ -49,12 +49,13 @@ if platform.system() == 'Linux':
     lat_30_i = T.nearest_search(lats, 30)
     nh_p = p[:,lat_30_i:,:]
     nh_x = x[:,lat_30_i:,:]
+    print(nh_x)
     mean_p_nh = np.mean(nh_p, axis=(1,2))
     mean_x_nh = np.mean(nh_x, axis=(1,2))
+    print(mean_x_nh)
 
     #save
     print(np.shape(mean_p_nh), np.shape(mean_x_nh))
-    print(mean_x_nh)
     exit()
     df = pd.DataFrame(columns=['pressure', 'var'], data=[mean_p_nh, mean_x_nh])
     print('saved to ' + os.path.join(root, 'lev_p.csv'))
