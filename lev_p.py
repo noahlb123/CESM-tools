4d3a53306c52fa18d3c6aa5d161b1893355b2d45
noahlb123 <noahliguoribills@gmail.com>
2025-01-13 12:26:32 -0500

    Update lev_p.py

--- lev_p.py
+++ lev_p.py
@@ -6,6 +6,13 @@ from netCDF4 import Dataset
 import numpy as np
 T = ToolBox()
 
+def evaluate(s):
+    l = len(s)
+    if s[l - 4:l] == ' && ':
+        s = s[0:l - 4]
+    os.system(s)
+    return 'cd ' + root + ' && '
+
 if len(sys.argv) < 4:
     raise Exception('3 command line arguments required: <root path> <file name> <target variable>')
 root = sys.argv[1]
@@ -13,5 +20,5 @@ file_name = sys.argv[2]
 target_v = sys.argv[3]
 
 to_eval = 'cd ' + root + ' && '
-
-to_eval += "ncap2 -O -s 'p=double(a*p0+b*ps);' " + file_name + ' ' + file_name
+to_eval += "ncap2 -O -s 'p=double(a*p0+b*ps);' " + file_name + ' ' + file_name + ' && '
+evaluate(to_eval)
