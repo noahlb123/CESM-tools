beaef01b4520f28ba67c2f126acd47e61c881569
noahlb123 <noahliguoribills@gmail.com>
2025-01-30 15:22:39 -0500

    Update new-workflow.sh

--- new-workflow.sh
+++ new-workflow.sh
@@ -12,11 +12,11 @@ do
 done
 
 #main data analysis
-python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all x 10 &&
-python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all CESM 10 &&
-python3 nco-pi-pd.py sootsn /glade/derecho/scratch/nlbills/all-ice-core-data/sootsn x 10 &&
-python3 nco-pi-pd.py loadbc /glade/derecho/scratch/nlbills/cmip-atmos x 10 &&
-python3 nco-pi-pd.py mmrbc /glade/derecho/scratch/nlbills/all-ice-core-data/mmrbc x 10 &&
+python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all x 10,10 &&
+python3 nco-pi-pd.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all CESM 10,10 &&
+python3 nco-pi-pd.py sootsn /glade/derecho/scratch/nlbills/all-ice-core-data/sootsn x 10,10 &&
+python3 nco-pi-pd.py loadbc /glade/derecho/scratch/nlbills/cmip-atmos x 10,10 &&
+python3 nco-pi-pd.py mmrbc /glade/derecho/scratch/nlbills/all-ice-core-data/mmrbc x 10,10 &&
 
 #timeseries
 python3 nco-timeseries.py drybc /glade/derecho/scratch/nlbills/cmip6-snow-dep/all &&
