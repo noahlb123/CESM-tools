600ef4b57fe37c7aa681326432d6ef4b58a25128
noahlb123 <noahliguoribills@gmail.com>
2025-01-30 14:06:47 -0500

    Update nco-pi-pd.py

--- nco-pi-pd.py
+++ nco-pi-pd.py
@@ -107,7 +107,7 @@ for model_name, d in main_dict.items():
             #time_index = T.nearest_search(times, year)
             i_end_decade = T.nearest_search(times, year + avg_window / 2)
             f.close()
-            print(times[i_start_decade], times[i_end_decade])
+            print(times[i_start_decade] - times[i_end_decade], times[i_start_decade], times[i_end_decade])
             new_filename = model_name + file_suffix + '.nc'
             to_eval += 'ncwa -b -a time -d time,' + str(i_start_decade) + ',' + str(i_end_decade) + ' ' + filename + ' ' + new_filename + ' -O && '
             #to_eval += 'ncks -d time,' + str(time_index) + ' ' + filename + ' ' + new_filename + ' -O && '
