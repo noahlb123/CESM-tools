1ce94e000fbcd16eb869f2988657963a0a23aa26
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 19:06:36 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -8,6 +8,11 @@ regional_mean = np.reshape(np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2
 
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
 
+print('time:')
+print(f['time'])
+print('bc')
+print(regional_mean[0])
+
 for i in range(8):
     plt.plot(f['time'], regional_mean[i], label=sector_dict[i])
 plt.legend()
