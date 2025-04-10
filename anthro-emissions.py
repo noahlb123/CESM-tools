cc2fa77414b8fcebee8fca3eefa6d77405d4f3d4
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 19:02:39 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -4,9 +4,11 @@ import numpy as np
 import os
 
 f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped.nc')
-regional_mean = np.reshape(np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2), (8, 1200))
+regional_mean = np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2)
 
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
 
-plt.stackplot(f['time'], regional_mean, labels=list(sector_dict.values()))
+for i in range(8):
+    plt.plot(f['time'], regional_mean[:,i], label=sector_dict[i])
+plt.legend()
 plt.savefig(os.path.join(os.getcwd(), 'stacked-anthro-emissions.png'), dpi=200)
