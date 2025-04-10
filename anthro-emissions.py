7dec4a3cc9db7db1c9c24d90ec6c996158a79b6f
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 19:00:04 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -9,4 +9,4 @@ regional_mean = np.reshape(np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
 
 plt.stackplot(f['time'], regional_mean, labels=list(sector_dict.values()))
-plt.savefig(os.getcwd(), dpi=200)
+plt.savefig(os.path.join(os.getcwd(), 'stacked-anthro-emissions.png'), dpi=200)
