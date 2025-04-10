23382a8e5854c1e42c883168caa44b4abed1e0e2
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 18:48:05 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -2,4 +2,4 @@ from netCDF4 import Dataset
 import numpy as np
 
 f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped.nc')
-print(np.shape(f['BC_em_anthro']))
+print(np.shape(np.mean(np.mean(f['BC_em_anthro'][:]), axis=3), axis=2))
