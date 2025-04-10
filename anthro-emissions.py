30a53851c9e6bcf97ae7ba73e020e016bb5b3b51
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 19:10:54 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -9,6 +9,6 @@ regional_mean = np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2)
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
 
 for i in range(8):
-    plt.plot(f['time'][:], regional_mean[:,i], label=sector_dict[i])
+    plt.plot(f['time'][:] / 365 + 1750, regional_mean[:,i], label=sector_dict[i])
 plt.legend()
 plt.savefig(os.path.join(os.getcwd(), 'stacked-anthro-emissions.png'), dpi=200)
