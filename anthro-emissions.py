0c36d355296869d3abf401417815fa86ae935150
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-10 18:59:06 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -1,6 +1,12 @@
 from netCDF4 import Dataset
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped.nc')
-print(np.shape(np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2)))
-#print(np.shape(np.mean(np.mean(f['BC_em_anthro'][:]), axis=3), axis=2))
+regional_mean = np.reshape(np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2), (8, 1200))
+
+sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
+
+plt.stackplot(f['time'], regional_mean, labels=list(sector_dict.values()))
+plt.savefig(os.getcwd(), dpi=200)
