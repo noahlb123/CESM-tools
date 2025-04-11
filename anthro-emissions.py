09bf48e5fad324b3b9a9ad365516c7c72dde5220
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-11 09:47:18 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -3,22 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-#f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped-na.nc')
-f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_190001-194912.nc')
-regional_mean = np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2)
+#crop to north america
+#ncks -d lat,7,90 -d lon,-179,-20 out.nc cropped-na.nc
+#and north hemisphere
+#ncks -d lat,0,90 -d lon,-180,180 out.nc cropped-nh.nc
+
+f_na = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped-na.nc')
+f_nh = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped-nh.nc')
 
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
 
-print(np.max(f['lat']), np.min(f['lat']), np.max(f['lon']), np.min(f['lon']))
-
 #compentnet
+na_regional_mean = np.mean(np.mean(f_na['BC_em_anthro'][:], axis=3), axis=2)
+
 for i in range(8):
-    plt.plot(f['time'][:] / 365 + 1750, regional_mean[:,i], label=sector_dict[i])
+    plt.plot(f_na['time'][:] / 365 + 1750, na_regional_mean[:,i], label=sector_dict[i])
 plt.legend()
 plt.title('North American CEDS BC Emissions')
 plt.xlabel('Year (CE)')
 plt.ylabel('BC (kg m-2 s-1)')
-plt.ylim((0, 1))
+plt.ylim()
 plt.savefig(os.path.join(os.getcwd(), 'component-anthro-emissions.png'), dpi=200)
 
 #combined
+na_sum = np.sum(np.mean(np.mean(f_na['BC_em_anthro'][:], axis=3), axis=2), axis=1)
+nh_sum = np.sum(np.mean(np.mean(f_nh['BC_em_anthro'][:], axis=3), axis=2), axis=1)
+plt.plot(f_na['time'][:] / 365 + 1750, na_sum, label='North America')
+plt.plot(f_nh['time'][:] / 365 + 1750, nh_sum, label='Northern Hemisphere')
+plt.legend()
+plt.title('CEDS BC Emissions')
+plt.xlabel('Year (CE)')
+plt.ylabel('BC (kg m-2 s-1)')
+plt.ylim()
+plt.savefig(os.path.join(os.getcwd(), 'combined-anthro-emissions.png'), dpi=200)
