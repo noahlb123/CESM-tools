63606edef80f504145d4387a0242f358b3763003
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-11 08:48:09 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped-na.nc')
+#f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/cropped-na.nc')
+f = Dataset('/glade/derecho/scratch/nlbills/ceds-anthro-emissions/BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_190001-194912.nc')
 regional_mean = np.mean(np.mean(f['BC_em_anthro'][:], axis=3), axis=2)
 
 sector_dict = {0: 'Agriculture', 1: 'Energy', 2: 'Industrial', 3: 'Transportation', 4: 'Residential, Commercial, Other', 5: 'Solvents production and application', 6: 'Waste', 7: 'International Shipping'}
