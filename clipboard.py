e2f20dd8aeae0fb49d595c08c6e47551dc6a4dab
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-16 12:34:48 -0400

    Update clipboard.py

--- clipboard.py
+++ clipboard.py
@@ -44,7 +44,8 @@ root = '/glade/campaign/cesm/collections/cesmLE/restarts'
 for sub_dir in os.listdir(root):
     if ('B20TRC5CNBDRD' in sub_dir) and (not 'OIC' in sub_dir):
         i = int(sub_dir.split('.')[-1])
-        for year in [1980, 1975, 1970, 1965, 1960, 1955]:
+        years = [1980, 1975, 1970, 1965, 1960, 1955] if i != 34 else [1982, 1977, 1972, 1970, 1965, 1960, 1955]
+        for year in years:
             path = os.path.join(root, sub_dir, 'b.e11.B20TRC5CNBDRD.f09_g16.0' + "{:02d}".format(i) + '.rest.' + str(year) + '-01-01-00000.tar')
             if not os.path.isfile(path):
                 print(path)
