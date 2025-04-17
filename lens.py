8647bb92a9ccc36606ddaaca2baafd2caf654b2d
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-17 15:00:10 -0400

    Update lens.py

--- lens.py
+++ lens.py
@@ -49,12 +49,13 @@ if sys.argv[1] == 'setup':
     for run, files in run_file_map.items():
         s += 'ncra ' + ' '.join(files) + ' ' + str(run) + '.nc && '
     print(s[0:len(s) - 4])
+    print('\n')
     #remove unneeded vars
     print('ncks -v bc_a1_SRF,hyai,hybi,P0,PSL,TREFHT,lat,lon pd-avg.nc pd-small.nc')
 
 
 
-elif sys.argv[1] == 'old':
+elif sys.argv[1] == 'old' or sys.argv[1] == 'old':
 
     system = platform.system() #differentiate local and derecho env by sys platform
     T = tools.ToolBox()
@@ -111,47 +112,64 @@ elif sys.argv[1] == 'old':
         'bc_c1']#these are depositions
 
     for mode in ['pd', 'pi']:
-        print(mode)
-        h = 1 if mode == "pd" else 0
-        #get viable models
-        for model in models:
-            if mode == 'pd':
-                years = [1980 + 5 * (x - 2) for x in range(5)] if model != 34 else (1977, 1982, 1987, 1972, 1970)
-            else:
-                years = (1865, 1855, 1860)
-            for year in years:
-                #get path
-                year_s = str(year) + "-01-01-00000"
-                working_path = '/glade/derecho/scratch/nlbills/lens-all-' + mode
-                file_paths = modelN2fnames(model, year_s, h)
-                print(file_paths)
+        if sys.argv[1] == 'old':
+            print(mode)
+            h = 1 if mode == "pd" else 0
+            #get viable models
+            for model in models:
+                if mode == 'pd':
+                    years = [1980 + 5 * (x - 2) for x in range(5)] if model != 34 else (1977, 1982, 1987, 1972, 1970)
+                else:
+                    years = (1865, 1855, 1860)
+                for year in years:
+                    #get path
+                    year_s = str(year) + "-01-01-00000"
+                    working_path = '/glade/derecho/scratch/nlbills/lens-all-' + mode
+                    file_paths = modelN2fnames(model, year_s, h)
+                    print(file_paths)
+                    continue
                 continue
-            continue
-        if 'cringe':
-                for file_path in file_paths:
-                    if system == "Darwin":
-                        path = os.path.join(working_path, "data", "model-ice-depo", "lens", file_path)
-                    else:
-                        path = os.path.join(working_path, file_path)
-                    if os.path.exists(path):
-                        #check if has bc vars
-                        f = Dataset(path)
-                        all_vars = f.variables
-                        model_index = str(model) + "-h" + file_path[file_path.index("cam.") + len("cam.") + 1] + year_s
-                        for v in vars:
-                            if v in all_vars:
-                                if model_index not in viable_models:
-                                    viable_models.append(model_index)
-                                if model_index not in model_path_map.keys():
-                                    model_path_map[model_index] = path
-                                if model_index not in model_year_map.keys():
-                                    model_year_map[model_index] = year
-                                if model_index in model_var_map:
-                                    model_var_map[model_index].append(v)
-                                else:
-                                    model_var_map[model_index] = [v]
-                        f.close()
+            if 'cringe':
+                    for file_path in file_paths:
+                        if system == "Darwin":
+                            path = os.path.join(working_path, "data", "model-ice-depo", "lens", file_path)
+                        else:
+                            path = os.path.join(working_path, file_path)
+                        if os.path.exists(path):
+                            #check if has bc vars
+                            f = Dataset(path)
+                            all_vars = f.variables
+                            model_index = str(model) + "-h" + file_path[file_path.index("cam.") + len("cam.") + 1] + year_s
+                            for v in vars:
+                                if v in all_vars:
+                                    if model_index not in viable_models:
+                                        viable_models.append(model_index)
+                                    if model_index not in model_path_map.keys():
+                                        model_path_map[model_index] = path
+                                    if model_index not in model_year_map.keys():
+                                        model_year_map[model_index] = year
+                                    if model_index in model_var_map:
+                                        model_var_map[model_index].append(v)
+                                    else:
+                                        model_var_map[model_index] = [v]
+                            f.close()
 
+        if sys.argv[1] == 'new':
+            root = '/glade/derecho/scratch/nlbills/lens/pd'
+            viable_models.append('pi-small.nc')
+            model_var_map[file] = ['bc_a1_SRF']
+            model_year_map = 1850
+            model_path_map[file] = os.path.join('/glade/derecho/scratch/nlbills/lens/pi', 'pi-small.nc')
+            for file in os.listdir(root):
+                if file.replace('.nc', '').isnumeric():
+                    viable_models.append(file)
+                    model_var_map[file] = ['bc_a1_SRF']
+                    model_year_map = 1980
+                    model_path_map[file] = os.path.join(root, file)
+
+
+
+        
         #get ice core lat, lon
         index_path = 'data/standardized-ice-cores/index.csv'
         dupe_path = 'data/standardized-ice-cores/index-dup-cores.csv'
