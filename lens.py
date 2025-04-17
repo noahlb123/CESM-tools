ff4a6d917ba756c7c55e66f48e222d12f6567d15
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-17 14:32:45 -0400

    Update lens.py

--- lens.py
+++ lens.py
@@ -48,7 +48,7 @@ if sys.argv[1] == 'setup':
     s = ''
     for run, files in run_file_map.items():
         s += 'ncra ' + ' '.join(files) + ' ' + str(run) + '.nc && '
-    print(s[0:len(s - 4)])
+    print(s[0:len(s) - 4])
     #remove unneeded vars
     print('ncks -v bc_a1_SRF,hyai,hybi,P0,PSL,TREFHT,lat,lon pd-avg.nc pd-small.nc')
 
