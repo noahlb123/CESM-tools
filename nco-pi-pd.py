41ea56770a97dd908be34c6da806a72972bc4a83
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-24 13:44:07 -0400

    Update nco-pi-pd.py

--- nco-pi-pd.py
+++ nco-pi-pd.py
@@ -234,6 +234,7 @@ for file in filenames:
 to_eval += 'echo "binning..." && '
 bases = []
 for base, files in bins.items():
+    print(base, files)
     if target_v == 'mmrbc':
         if 'CESM2-WACCM_re.nc' in files:
             files.remove('CESM2-WACCM_re.nc')
