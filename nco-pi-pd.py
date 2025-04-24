c818e0fd3d96d5986a1dd80c19faf6b30f614f25
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-04-24 10:10:44 -0400

    Update nco-pi-pd.py

--- nco-pi-pd.py
+++ nco-pi-pd.py
@@ -36,7 +36,7 @@ def setup():
         for file in files:
             if ('wetbc' in file or 'drybc' in file) and 'CanESM5' in file:
                 f = Dataset(os.path.join(root, file))
-                if ('lat' not in f.variables.keys()) or ('lon' not in f.variables.keys()):
+                if (not 'lat' in f.variables.keys()) or (not 'lon' in f.variables.keys()):
                     print(file, list(f.variables.keys()))
                 f.close()
 
