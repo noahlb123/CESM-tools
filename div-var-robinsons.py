75d325212bc769ae6324098a2492136616bdff53
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-09 10:33:02 -0400

    Update div-var-robinsons.py

--- div-var-robinsons.py
+++ div-var-robinsons.py
@@ -12,6 +12,8 @@ T = tools.ToolBox()
 root = '/glade/derecho/scratch/nlbills/all-ice-core-data'
 work_dir = os.path.join(root, 'ratios')
 dirs = ['loadbc', 'mmrbc', 'sootsn', 'wet-dry']
+name_var_map = {'loadbc': 'loadbc', 'mmrbc': 'mmrbc', 'sootsn': 'sootsn', 'wet-dry': 'drybc'}
+file2dir = {}
 
 if step == '1' or step == 'a': #combine nc files
     columns = []
@@ -25,26 +27,26 @@ if step == '1' or step == 'a': #combine nc files
             index.append(dir)
     df_mult = pd.DataFrame(columns=columns, index=index)
     df_div = pd.DataFrame(columns=columns, index=index)
+    smallest_grid = T.smallest_grid(columns)
+    #regrid
+    old_new_name = {}
+    for dir in columns:
+        new_name = os.path.join(work_dir, dir, 'CESM2.nc')
+        old_name = os.path.join(root, dir, 'CESM2.nc')
+        old_new_name[old_name] = new_name
+        os.system('ncremap -d ' + smallest_grid + ' ' + old_name + ' ' + new_name + ' -O')
+        #rename var
+        os.system('ncrename -h -O -v ' + name_var_map[dir] + ',' + 'X' + ' ' + old_name)
     for numo in columns:
         for deno in index:
             if numo == deno:
                 df_mult.loc[deno, numo] = os.path.join(root, dir, 'CESM2.nc')
             else:
-                nums = [os.path.join(root, numo, 'CESM2_' + era + '.nc') for era in ('pi', 'pd')]
-                denos = [os.path.join(root, deno, 'CESM2_' + era + '.nc') for era in ('pi', 'pd')]
-                smallest_grid = T.smallest_grid(nums + denos)
-                name_var_map = {}
-                for file in nums + denos:
-                    f = Dataset(file)
-                    vars = list(f.variables.keys())
-                    for v in vars:
-                        print(v)
-                '''
-                #rename var
-                t_var = 
-                os.system('ncrename -h -O -v wetbc,drybc ' + p_suffix + ' && ')
+                numo_path = os.path.join(work_dir, numo, 'CESM2.nc')
+                deno_path = os.path.join(work_dir, deno, 'CESM2.nc')
+                new_path = os.path.join(work_dir, deno, numo + '_X_' + deno + '.nc')
                 #math
-                'ncbo --op_typ=' + operation + ' ' + m_suffix + ' ' + p_suffix + ' ' + new_name + '.nc -O && '
-                df_mult.loc[deno, numo] = os.path.join(root, dir, 'CESM2.nc')
+                'ncbo --op_typ=multiply ' + numo_path + ' ' + deno_path + ' ' + new_path + '.nc -O && '
+                df_mult.loc[deno, numo] = new_path
 if step == '2' or step == 'a': #plot
-    pass'''
+    pass
