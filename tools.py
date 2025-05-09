e24604885389975a4aad9afaa104ab6351b84e52
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-09 10:07:02 -0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -276,8 +276,8 @@ class ToolBox:
         else:
             raise Exception('Type of dir not recognised: ' + str(dir) + ':' + str(type(dir)))
         for file in files:
-            print(file, f_param)
-            if qual_f('file:', file, 'f_param:', f_param):
+            print('file:', file, 'param:', f_param)
+            if qual_f(file, f_param):
                 path = os.path.join(dir, file) if type(dir) == '' else file
                 try:
                     f = Dataset(path)
