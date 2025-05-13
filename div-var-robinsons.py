ea7963bc87bc9c1e68059cb968e47f5fbbc8466c
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-13 17:46:01 -0400

    Update div-var-robinsons.py

--- div-var-robinsons.py
+++ div-var-robinsons.py
@@ -93,7 +93,7 @@ if step == '2' or step == 'a': #plot
                 if numo == deno:
                     filename = os.path.join(root, numo, 'CESM2.nc')
                 else:
-                    filename = numo + '_' + op + '_' + deno + '.nc'
+                    filename = os.path.join(work_dir, numo + '_' + op + '_' + deno + '.nc')
                 
                 #title
                 if numo_i == 0:
@@ -105,7 +105,7 @@ if step == '2' or step == 'a': #plot
                 ax[numo_i, deno_i].add_feature(cartopy.feature.COASTLINE, edgecolor='black')
 
                 #get data
-                f = Dataset(os.path.join(work_dir, filename))
+                f = Dataset(filename)
                 lats = f['lat'][:]
                 lons = f['lon'][:]
                 x = f['X'][0,0,:,:] if 'mmrbc' in filename else f['X'][0,:,:]
