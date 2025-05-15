d4d6ddf3b05b4a201ecfad8ab018d99c6a7c7da9
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-15 13:16:38 -0400

    Update div-var-robinsons.py

--- div-var-robinsons.py
+++ div-var-robinsons.py
@@ -111,7 +111,7 @@ if step == '2' or step == 'a': #plot
                     filename = os.path.join(work_dir, numo + '_' + op + '_' + deno + '.nc')
                     f_numo = Dataset(os.path.join(work_dir, numo + '.nc'))
                     f_deno = Dataset(os.path.join(work_dir, deno + '.nc'))
-                    print(f_numo, op, f_deno)
+                    print(os.path.join(work_dir, numo + '.nc'), op, os.path.join(work_dir, deno + '.nc'))
                     lats = f_numo['lat'][:]
                     lons = f_numo['lon'][:]
                     x_n = zscore(f_numo['X'][0,0,:,:]) if 'mmrbc' in numo else zscore(f['X'][0,:,:])
