572b9efada3092a3f624783da176272f2e46be53
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-15 12:24:46 -0400

    Update div-var-robinsons.py

--- div-var-robinsons.py
+++ div-var-robinsons.py
@@ -111,20 +111,20 @@ if step == '2' or step == 'a': #plot
 
                 #color
                 cmap = colormaps['BrBG_r'] if op == 'D' else colormaps['viridis']
-                min, max = np.nanpercentile(np.ma.masked_invalid(x), (0.7, 0.9))
+                min, max = np.nanpercentile(np.ma.masked_invalid(x), (0.1, 0.9))
                 if filename == '/glade/derecho/scratch/nlbills/all-ice-core-data/loadbc/CESM2.nc':
                     min = 0.8
                     max = np.nanmax(np.ma.masked_invalid(x))
                 if op == 'D':
                     max = np.nanmax(np.ma.masked_invalid(x))
-                print(min, max)
+                print(np.nanmin(np.ma.masked_invalid(x)), min, max)
                 c_norm = LogNorm(vmin=1/max, vmax=max) if op == 'D' else LogNorm(vmin=min, vmax=max)
                 sm = ScalarMappable(cmap=cmap, norm=c_norm)
 
                 #plot
                 ax[numo_i, deno_i].pcolormesh(lons, lats, x, cmap=cmap, norm=c_norm, transform=cartopy.crs.PlateCarree())
         cbar = plt.colorbar(mappable=ScalarMappable(cmap=cmap, norm=LogNorm(vmin=0.1, vmax=10)), orientation="horizontal", ax=ax, extend='both', ticks=(0.1, 1, 10))
-        labels = ('1/Max', '1', 'Max') if op == 'D' else ('80th Percentile', '', '90th Percentile')
+        labels = ('1/Max', '1', 'Max') if op == 'D' else ('10th Percentile', '', '90th Percentile')
         cbar.ax.set_xticklabels(labels)
         plt.savefig(os.path.join(os.getcwd(), op + '.png'), dpi=200)
         print('saved to ' + os.path.join(os.getcwd(), op + '.png'))
