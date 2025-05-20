65fae96535089ce0c8456cebb947ae3d6367230b
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-20 13:22:12 -0400

    Update lens-bias.py

--- lens-bias.py
+++ lens-bias.py
@@ -14,22 +14,20 @@ files = sys.argv[1:]
 index_path = 'data/standardized-ice-cores/index.csv'
 dupe_path = 'data/standardized-ice-cores/index-dup-cores.csv'
 ice_coords = T.get_ice_coords(index_path, dupe_path)
-df = pd.DataFrame(columns=files + list(ice_coords.keys()))
+df = pd.DataFrame(index=files, columns=list(ice_coords.keys()))
 lens_data = pd.read_csv('data/model-ice-depo/lens/lens.csv')
 lens_data = lens_data.set_index('Unnamed: 0').drop(['pi'], axis=0).mean(axis=0)
-print(lens_data)
-exit()
 
 for file in files:
     f = Dataset(file)
     lats, lons = T.adjust_lat_lon_format(f['lat'][:], f['lon'][:])
     times = f['time']
-    v = f['X'][:]
+    v = f['X'][0,0,:,:] if 'mmrbc' in file else f['X'][0,:,:]
     for core_name in ice_coords.keys():
         y, x = ice_coords[core_name]
         lat = T.nearest_search(lats, y)
         lon = T.nearest_search(lons, x)
-        lens_data[core_name] = lens_data[core_name] * v[0, lat, lon]
+        df.loc[file, core_name] = lens_data[core_name] * v[lat, lon]
     f.close()
 
-lens_data.to_csv(os.path.join(os.getcwd(), 'lens-bias.csv'))
+df.to_csv(os.path.join(os.getcwd(), 'lens-bias.csv'))
