bf5fc7ca1087981c7cacbed26736f8fa83283bf1
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-05-27 17:09:00 -0400

    Update anthro-emissions.py

--- anthro-emissions.py
+++ anthro-emissions.py
@@ -185,7 +185,7 @@ elif mode == 'r': #ratios plotted on robinson globe
     for col_i in range(col_n):
         for row_i in range(row_n):
             ax = axes[row_i, col_i]
-            if col_i * 2 + row_i < 5:
+            if col_i * 2 + row_i < 3:
                 key = i_d_map[col_i * 2 + row_i]
                 arr = final_mats[key]
                 lat = ncdf_dict[key.split('+')[0].lower() + '-pd']['lats']
