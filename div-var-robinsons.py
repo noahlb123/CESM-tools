aeb418f009df9346b05a705afd3b7f88dd2a65e3
Noah Liguori-Bills <noahliguoribills@gmail.com>
2025-09-03 08:36:11 -0400

    Update div-var-robinsons.py

--- div-var-robinsons.py
+++ div-var-robinsons.py
@@ -117,7 +117,7 @@ if step == '2' or step == 'a': #plot
                 if numo_i == 0:
                     ax[numo_i, deno_i].set_title(title_map[deno], size=9)
                 if deno_i == 0:
-                    v_mod = -0.2 if numo != 'sootsn' else 0.3
+                    v_mod = 0.3 if numo != 'wet-dry' else 0.1
                     ax[numo_i, deno_i].text(-0.2, v_mod, title_map[numo], size=9, horizontalalignment='center', rotation=90, transform=ax[numo_i, deno_i].transAxes)
                 
                 #setup cartopy
